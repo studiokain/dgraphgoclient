733483da845913754d754a01ff6926bbc4f6c6ac
Pawan Rawal <pawan@dgraph.io>
2016-06-20 12:36:09 +0530

    adding go client

---
+++ dgraphgoclient/README.md
@@ -0,0 +1,5 @@
+This folder contains example code for using the Go client as well as the proto
+buffer file for connecting to the server.
+
+The `graphresponse.pb.go` file is generated from
+https://github.com/dgraph-io/dgraph/blob/master/query/graph/graphresponse.proto.
---
+++ dgraphgoclient/main.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 DGraph Labs, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	"github.com/dgraph-io/clients/dgraphgoclient/graph"
+)
+
+var ip = flag.String("ip", "127.0.0.1:8081", "Port to communicate with server")
+var q = flag.String("query", "", "Query sent to the server")
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*ip, grpc.WithInsecure())
+	if err != nil {
+		log.Fatal("DialTCPConnection")
+	}
+	defer conn.Close()
+
+	c := graph.NewDGraphClient(conn)
+
+	resp, err := c.Query(context.Background(), &graph.Request{Query: *q})
+	if err != nil {
+		log.Fatalf("Error in getting response from server, %s", err)
+	}
+
+	fmt.Printf("Subgraph %+v", resp.N)
+}
